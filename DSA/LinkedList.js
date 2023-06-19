b27b423e7764627d08eaad7e63e77950c089a251
sailesh <sailehadhikari72@gmail.com>
2023-06-19 09:13:03 +0545

    dsa day-2

---
+++ DSA/LinkedList.js
@@ -0,0 +1,1 @@
+// Given an array of integers, write a function in JavaScript that finds the two numbers whose sum is closest to zero. The function should return an array containing these two numbers.
